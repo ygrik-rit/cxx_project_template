1ccbafae42703924488edeb208aa0c743e66c91e
Stepanov Igor <i.stepanov@rit.va>
2026-01-16 10:01:52 +0300

    my try

--- example/example_sdl.cxx
+++ example/example_sdl.cxx
@@ -1,53 +1,114 @@
-// Example program:
-// Using SDL3 to create an application window
+/*
+ * Logic implementation of the Snake game. It is designed to efficiently
+ * represent the state of the game in memory.
+ *
+ * This code is public domain. Feel free to use it for any purpose!
+ */
 
+#define SDL_MAIN_USE_CALLBACKS 1 /* use the callbacks instead of main() */
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
 
-int main(int argc, char* argv[])
+using AppState = struct
+{
+    SDL_Window*   window;
+    SDL_Renderer* renderer;
+    Uint64        last_step;
+};
+
+SDL_AppResult SDL_AppIterate(void* appstate)
+{
+    AppState* as = (AppState*)appstate;
+
+    SDL_SetRenderDrawColor(as->renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
+    SDL_RenderClear(as->renderer);
+
+    SDL_SetRenderDrawColor(
+        as->renderer, 255, 255, 0, SDL_ALPHA_OPAQUE); /*head*/
+    SDL_RenderPresent(as->renderer);
+    return SDL_APP_CONTINUE;
+}
+
+static const struct
+{
+    const char* key;
+    const char* value;
+} extended_metadata[] = { { SDL_PROP_APP_METADATA_URL_STRING,
+                            "https://examples.libsdl.org/SDL3/demo/01-snake/" },
+                          { SDL_PROP_APP_METADATA_CREATOR_STRING, "SDL team" },
+                          { SDL_PROP_APP_METADATA_COPYRIGHT_STRING,
+                            "Placed in the public domain" },
+                          { SDL_PROP_APP_METADATA_TYPE_STRING, "game" } };
+
+SDL_AppResult SDL_AppInit(void** appstate, int argc, char* argv[])
+{
+    size_t i;
+
+    if (!SDL_SetAppMetadata("Example Snake game", "1.0", "com.example.Snake"))
+    {
+        return SDL_APP_FAILURE;
+    }
+
+    for (i = 0; i < SDL_arraysize(extended_metadata); i++)
+    {
+        if (!SDL_SetAppMetadataProperty(extended_metadata[i].key,
+                                        extended_metadata[i].value))
+        {
+            return SDL_APP_FAILURE;
+        }
+    }
+
+    if (!SDL_Init(SDL_INIT_VIDEO))
+    {
+        SDL_Log("Couldn't initialize SDL: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+
+    AppState* as = (AppState*)SDL_calloc(1, sizeof(AppState));
+    if (!as)
+    {
+        return SDL_APP_FAILURE;
+    }
+
+    *appstate = as;
+
+    if (!SDL_CreateWindowAndRenderer("examples/demo/snake",
+                                     100,
+                                     100,
+                                     SDL_WINDOW_RESIZABLE,
+                                     &as->window,
+                                     &as->renderer))
+    {
+        return SDL_APP_FAILURE;
+    }
+    SDL_SetRenderLogicalPresentation(
+        as->renderer, 100, 100, SDL_LOGICAL_PRESENTATION_LETTERBOX);
+
+    as->last_step = SDL_GetTicks();
+
+    return SDL_APP_CONTINUE;
+}
+
+SDL_AppResult SDL_AppEvent(void* appstate, SDL_Event* event)
 {
 
-    SDL_Window* window; // Declare a pointer
-    bool        done = false;
-
-    SDL_Init(SDL_INIT_VIDEO); // Initialize SDL3
-
-    // Create an application window with the following settings:
-    window = SDL_CreateWindow("An SDL3 window", // window title
-                              640,              // width, in pixels
-                              480,              // height, in pixels
-                              SDL_WINDOW_OPENGL // flags - see below
-    );
-
-    // Check that the window was successfully created
-    if (window == NULL)
+    switch (event->type)
     {
-        // In the case that the window could not be made...
-        SDL_LogError(SDL_LOG_CATEGORY_ERROR,
-                     "Could not create window: %s\n",
-                     SDL_GetError());
-        return 1;
+        case SDL_EVENT_QUIT:
+            return SDL_APP_SUCCESS;
+        default:
+            break;
     }
+    return SDL_APP_CONTINUE;
+}
 
-    while (!done)
+void SDL_AppQuit(void* appstate, SDL_AppResult result)
+{
+    if (appstate != NULL)
     {
-        SDL_Event event;
-
-        while (SDL_PollEvent(&event))
-        {
-            if (event.type == SDL_EVENT_QUIT)
-            {
-                done = true;
-            }
-        }
-
-        // Do game logic, present a frame, etc.
+        AppState* as = (AppState*)appstate;
+        SDL_DestroyRenderer(as->renderer);
+        SDL_DestroyWindow(as->window);
+        SDL_free(as);
     }
-
-    // Close and destroy the window
-    SDL_DestroyWindow(window);
-
-    // Clean up
-    SDL_Quit();
-    return 0;
 }
